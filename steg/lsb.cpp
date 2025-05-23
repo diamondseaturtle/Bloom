677b60a09a74db937641e717f95288635b22c4c8
Claire Wang <diamondseaturtle@gmail.com>
2025-05-23 11:09:17 -0700

    lsb skeleton

--- steg/lsb.cpp
+++ steg/lsb.cpp
@@ -6,7 +6,36 @@
 #include <sstream> 
 #include <fstream>
 
+using namespace std;
+
+bool Encode() {
+// twiddle bits
+}
+
+bool Decode() {
+// somehow get the file from the image
+}
+
+bool Check() {
+// does file fit into image?
+}
+
+int MBits() {
+ // extract msg file bits
+}
+
+int IBits() {
+// extract image bits
+}
+
+void Usage(char* prog) {
+    // not how it should work
+    cout << "Usage: " << prog << " [options] [file.txt] [img.png/jpg]" << endl 
+        << "    Options: " << endl << 
+            "       -e     Encode message into image" << endl << 
+            "       -d    Decode message from image" << endl;
+}
 
 int main(int argc, char* argv[]) {
-    
+
 }
